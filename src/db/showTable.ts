2cf6bb5e7f9d1159d7d15ee76406293c395c549a
rohitranjan-2702 <rohitranjan2702@gmail.com>
2025-09-03 13:08:31 +0530

    scripts added

--- src/db/showTable.ts
+++ src/db/showTable.ts
@@ -3,6 +3,11 @@ import db from './index';
 function showTable() {
   try {
     const result = db.prepare('SELECT * FROM guild_settings').all();
+    if (result.length === 0) {
+      console.log('No data found in guild_settings table');
+      return;
+    }
+
     console.log(result);
   } catch (error) {
     throw new Error('Failed to show table' + error);
